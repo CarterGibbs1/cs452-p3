4fa803d9b5d6da8a9fda845671a79a07f41a7ff2
CarterGibbs1 <97470347+CarterGibbs1@users.noreply.github.com>
2024-10-24 19:11:13 +0000

    Basic case working, some bugs to fix

--- src/lab.c
+++ src/lab.c
@@ -1,6 +1,17 @@
 #include <stdlib.h>
 #include <sys/time.h> /* for gettimeofday system call */
+#include <pthread.h>
+#include <math.h>
+#include <stdio.h>
 #include "lab.h"
+
+typedef struct mergesort_s_args {
+    void *A;
+    int p;
+    int r;
+} mergesort_s_args;
+
+void* mergesort_mt_to_s(void *);
 
 /**
  * @brief Standard insertion sort that is faster than merge sort for small array's
@@ -26,6 +37,32 @@ static void insertion_sort(int A[], int p, int r)
     }
 }
 
+void mergesort_mt(int A[], int n, int num_thread) {
+    // Create threads
+    pthread_t threads[num_thread];
+    mergesort_s_args *arguments = (mergesort_s_args*) malloc(sizeof(mergesort_s_args));
+    int chunk_size = floor(n / num_thread);
+    for (int i = 0; i < num_thread; i++) {
+        arguments->A = A;
+        arguments->p = i * chunk_size;
+        arguments->r = (i+1) * chunk_size - 1;
+        pthread_create(&threads[i], NULL, mergesort_mt_to_s, (void *) arguments);
+    }
+
+    // Join threads
+    for (int i = 0; i < num_thread; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    free(arguments);
+}
+
+void* mergesort_mt_to_s(void *arguments) {
+    mergesort_s_args *mergesort_args = (mergesort_s_args*) arguments;
+    //printf("%d, %d\n", mergesort_args->p, mergesort_args->r);
+    mergesort_s(mergesort_args->A, mergesort_args->p, mergesort_args->r);
+    return NULL;
+}
 
 void mergesort_s(int A[], int p, int r)
 {
