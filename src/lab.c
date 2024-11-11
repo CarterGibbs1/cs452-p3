1bc36345c24097fff19a411130c0169b84f3828c
CarterGibbs1 <97470347+CarterGibbs1@users.noreply.github.com>
2024-11-11 02:02:12 +0000

    Finished code

--- src/lab.c
+++ src/lab.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 #include <math.h>
 #include <stdio.h>
+#include <limits.h>
 #include "lab.h"
 
 // Debug methods
@@ -13,6 +14,12 @@ typedef struct mergesort_s_args {
     int p;
     int r;
 } mergesort_s_args;
+
+typedef struct merge_arguments {
+    int n;  // size of array
+    int num_arrays; // equal to num threads
+    int* array;
+} merge_arguments;
 
 void* mergesort_mt_to_s(void *);
 
@@ -40,13 +47,69 @@ static void insertion_sort(int A[], int p, int r)
     }
 }
 
+void merge_sorted_arrays(merge_arguments *args) {
+    // Have array of integers, related to pointer of index
+    // Check which index's value is smallest, append to new array
+    // Have method to check if index is out of bound
+
+    // Allocate return array
+    int curr = 0;
+    int *retVal = (int *) malloc(args->n * sizeof(int));
+
+    // Allocate index array
+    int *indexes = (int *) malloc(args->num_arrays * sizeof(int));
+    int chunk_size = args->n / args->num_arrays;
+    for (int i = 0; i < args->num_arrays; i++) {
+        indexes[i] = floor(i * chunk_size);
+    }
+
+    // Loop through each index to find which element is smallest
+    for (int i = 0; i < args->n; i++) {
+        int smallestElement = INT_MAX;
+        int smallestIndex = -1;
+        for (int j = 0; j < args->num_arrays; j++) {
+            int idx = indexes[j];
+
+            // If index is out of bounds // Array already merged
+            if (idx >= args->n || ((j < args->num_arrays - 1) && (idx >= floor((j + 1) * chunk_size)))) {
+                continue;
+            }
+
+            int element = args->array[idx];
+            if (element < smallestElement) {
+                smallestElement = element;
+                smallestIndex = j;
+            }
+        }
+
+        /**
+        printArr(retVal, args->n);
+        printf("curr: %i\n", curr);
+        printf("smallest element: %i\n", smallestElement);
+        printf("smallest index: %i\n", smallestIndex);
+        */
+
+        retVal[curr] = smallestElement;
+        curr++;
+        indexes[smallestIndex]++;
+    }
+
+
+    // copy merged output from retVal back to array
+    for (int i = 0; i < args->n; i++) {
+        args->array[i] = retVal[i];
+    }
+
+    // Free memory
+    free(retVal);
+    free(indexes);
+}
+
 void mergesort_mt(int A[], int n, int num_thread) {
     // Create threads
     pthread_t threads[num_thread];
     mergesort_s_args **arguments = (mergesort_s_args**) malloc(num_thread * sizeof(mergesort_s_args*));
     int chunk_size = n / num_thread;
-    printf("Start:\n");
-    printArr(A, n);
     for (int i = 0; i < num_thread; i++) {
         arguments[i] = (mergesort_s_args*) malloc(sizeof(mergesort_s_args));
         arguments[i]->A = A;
@@ -57,22 +120,30 @@ void mergesort_mt(int A[], int n, int num_thread) {
     }
 
     // Join threads
-    int p, q, r;
+    //int p, q, r;
     for (int i = 0; i < num_thread; i++) {
         pthread_join(threads[i], NULL);
         free(arguments[i]);
 
+        /**
         if (i == 0) continue;
         p = 0;
         q = i * chunk_size;
         r = (i != num_thread - 1) ? floor((i+1) * chunk_size - 1) : n - 1;
 
         merge_s(A, p, q, r);
+         */
     }
 
-    printf("End:\n");
-    printArr(A, n);
+    merge_arguments *merge_args = (merge_arguments *) malloc(sizeof(merge_arguments));
+    merge_args->array = A;
+    merge_args->n = n;
+    merge_args->num_arrays = num_thread;
+
+    merge_sorted_arrays(merge_args);
+
     free(arguments);
+    free(merge_args);
 }
 
 void* mergesort_mt_to_s(void *arguments) {
